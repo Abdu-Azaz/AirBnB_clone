cadec61bb9fc233ed0bdb3d562797c2255e306e9
Abdu-Azaz <abderrahmane.azaz@edu.uiz.ac.ma>
2024-03-09 04:06:07 -0800

    1st commit

---
+++ console.py
@@ -0,0 +1,19 @@
+import cmd
+
+class HelloWorld(cmd.Cmd):
+    """Simple command processor example."""
+
+    prompt = '(hbnb) '
+
+    def do_quit(self, line):
+        return True
+
+   
+    def do_EOF(self, line):
+        return True
+    def postcmd(self, stop, line):
+        """Called after a command is executed."""
+        return stop
+    
+if __name__ == '__main__':
+    HelloWorld().cmdloop()
